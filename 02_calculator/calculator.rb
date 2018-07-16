e2942d3ca33e55255442bc2cfc981fde9330d583
Tyler Linquata <tylerlinquata@gmail.com>
2018-07-16 16:44:06 -0600

    Complete exercise 02 and 03

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,14 @@
 #write your code here
+def add(a, b)
+	a + b
+end
+
+def subtract(a, b)
+	a - b
+end
+
+def sum(numbers)
+	sum = 0
+	numbers.each { |number| sum += number}
+	sum.to_i
+end
--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,32 @@
 #write your code here
+def echo(phrase)
+	phrase
+end
+
+def shout(phrase)
+	phrase.upcase
+end
+
+def repeat(phrase, num = 1)
+	result = phrase + ' ' + phrase
+	num -= 2
+	num.times do
+		result = result + ' ' + phrase
+	end
+	result
+end
+
+def start_of_word(word, num_letters = 1)
+	result = word[0, num_letters]
+end
+
+def first_word(string)
+	arr = string.split(' ')
+	arr[0]
+end
+
+def titleize(string) 
+	arr = string.split(' ')
+	arr.each { |word| word.capitalize }
+	arr
+end
